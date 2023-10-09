61e61d328bfdb257f32c6f9be31430d0aa45cdb5
Malika Oubilla <malikaoubilla@gmail.com>
2023-10-09 09:19:45 -0700

    10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,5 +4,5 @@ def divisible_by_2(my_list=[]):
         return None
     div = []
     for i in my_list:
-        div.append(True if i%2 == 0 else False)
+        div.append(True if i % 2 == 0 else False)
     return div
