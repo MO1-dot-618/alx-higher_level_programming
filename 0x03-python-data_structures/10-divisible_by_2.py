2963ce0707b0fb0d9ffb5bed8a94b64ce4b88230
Malika Oubilla <malikaoubilla@gmail.com>
2023-10-09 09:18:44 -0700

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    div = []
+    for i in my_list:
+        div.append(True if i%2 == 0 else False)
+    return div
