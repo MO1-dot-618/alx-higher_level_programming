53636d6fea4900c6dc7171b788cb6ba9f3fa5621
Malika Oubilla <malikaoubilla@gmail.com>
2023-10-24 07:53:50 -0700

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""task 0"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns square"""
+        return (self.__size ** 2)
