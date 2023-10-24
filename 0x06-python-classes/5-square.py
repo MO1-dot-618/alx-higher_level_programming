d64ea875d6729b0d79e56a0bff18bdba93c8526d
Malika Oubilla <malikaoubilla@gmail.com>
2023-10-24 08:24:52 -0700

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """ getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Setter for private instance attribute size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints square to stdout """
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print("")
