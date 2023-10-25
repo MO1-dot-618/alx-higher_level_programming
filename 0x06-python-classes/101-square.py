b73e1aa7e00535b9652899d88c8e73f8c8aa7b2e
Malika Oubilla <malikaoubilla@gmail.com>
2023-10-25 06:16:36 -0700

    101

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new Square"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """ getter """
+        return self.__size
+
+    @property
+    def position(self):
+        """ getter for position"""
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """ Setter for private instance attribute size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """ setter for position"""
+        if (not isinstance(value[0], int) or not isinstance(value[1], int)
+                or len(value) != 2 or not isinstance(value, tuple)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """returns square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints square to stdout """
+        if self.size == 0:
+            print("")
+        else:
+            for y in range(self.position[1]):
+                print("")
+            for i in range(self.size):
+                for x in range(self.position[0]):
+                    print(" ", end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print("")
+
+    def __str__(self):
+        """Define the print() """
+        strn = ""
+        if self.size == 0:
+            strn = strn + "\n"
+        else:
+            for y in range(self.position[1]):
+                strn = strn + "\n"
+            for i in range(self.size):
+                for x in range(self.position[0]):
+                    strn = strn + " "
+                for j in range(self.size):
+                    strn = strn + "#"
+                if i != (self.size - 1):
+                    strn = strn + "\n"
+        return strn
