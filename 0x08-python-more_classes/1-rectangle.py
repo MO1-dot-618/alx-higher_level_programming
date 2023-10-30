3567ad4ee2029ec33e8bf5c54f600aa83b088e95
Malika Oubilla <malikaoubilla@gmail.com>
2023-10-30 04:25:29 -0700

    1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+
+
+class Rectangle:
+    """Represent a Rectangle """
+    def __init__(self, width=0, height=0):
+        """Initialize a new instance"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for private instance attribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getter """
+        return self.__size
+
+    @height.setter
+    def height(self, value):
+        """ Setter for private instance attribute height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
