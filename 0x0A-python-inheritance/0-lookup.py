2c039cb5b2ad13f93f7a1b69014bebc8792fec6c
Malika Oubilla <malikaoubilla@gmail.com>
2023-11-06 05:54:40 -0800

    01

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" function to return methods"""
+
+def lookup(obj):
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" child of list class"""
+class MyList(list):
+    """class"""
+
+    def print_sorted(self):
+        print(self.sort())
