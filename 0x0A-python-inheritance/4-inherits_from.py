7643ed733aac387e72e9799a05b0310d637c31b1
Malika Oubilla <malikaoubilla@gmail.com>
2023-11-06 07:33:03 -0800

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,9 +2,9 @@
 """ function to return methods"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """ type """
 
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
