eecf58b2f230c7b4d6f8734cfe7f08165c6b0497
Malika Oubilla <malikaoubilla@gmail.com>
2023-11-06 06:39:41 -0800

    1.4

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -3,11 +3,7 @@ MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
 
-my_list.append(float('inf'))
-my_list.append('nan')
+my_list.append('string')
 my_list.append(8)
-my_list.append(-6)
 
-print(my_list)
 my_list.print_sorted()
-print(my_list)
