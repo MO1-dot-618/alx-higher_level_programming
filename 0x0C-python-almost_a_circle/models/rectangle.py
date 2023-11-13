e49d617f921c249f3ca43dd8e926d13a854fdf87
Malika <malikaoubilla@gmail.com>
2023-11-13 06:36:11 -0800

    tasks 1-7 without tests

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+""" Rectangle """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ class inherited from Base """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new instance"""
+        super().__init__(id) 
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for private instance attribute width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter for private instance attribute height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ getter """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter for private instance attribute x """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ getter """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter for private instance attribute y """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Return the area of the Rectangle"""
+        return (self.width * self.height)
+
+    def display(self):
+        """prints rectangle to stdout """
+        for y in range(self.y):
+            print("")
+        for i in range(self.height):
+            for x in range(self.x):
+                print(" ", end="")
+            for j in range(self.width):
+                print("#", end="")
+            print("")
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] (" + str(self.id) + ") "
+        string += str(self.__x) + "/" + str(self.__y) + " - "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
