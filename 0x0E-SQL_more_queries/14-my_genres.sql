11c4f8078183c198f105854401333391437ecff4
Malika Oubilla <malikaoubilla@gmail.com>
2023-12-13 09:37:43 -0800

    14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- genres of Dexter
+SELECT g.name AS name
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS t
+ON t.genre_id = g.id
+INNER JOIN tv_shows AS s
+ON s.id = g.show_id
+WHERE s.title = 'Dexter'
+ORDER BY name;
