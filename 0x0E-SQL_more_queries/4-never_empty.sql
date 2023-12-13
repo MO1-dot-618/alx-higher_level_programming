44ba5cb6adf8ecffafb980cd922dbf55e74762bb
Malika Oubilla <malikaoubilla@gmail.com>
2023-12-13 07:24:24 -0800

    4, 5

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- create table
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256)
+);
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- create table
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256),
+    PRIMARY KEY (id)
+);
