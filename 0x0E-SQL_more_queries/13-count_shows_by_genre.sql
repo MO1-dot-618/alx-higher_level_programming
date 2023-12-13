972428e91d62f2cbf9cf14553bcf9afec93b98f1
Malika Oubilla <malikaoubilla@gmail.com>
2023-12-13 09:18:22 -0800

    13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT g.name AS genre, COUNT(g.name) AS number_of_shows
 FROM tv_genres AS g
 INNER JOIN tv_show_genres AS t
 ON t.genre_id = g.id
-GROUP BY g.id
-ORDER BY number_of_shows;
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
