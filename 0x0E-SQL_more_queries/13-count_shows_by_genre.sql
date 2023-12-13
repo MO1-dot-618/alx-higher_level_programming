0505e0ad28cd587bbb2a3f4e03809757345d32de
Malika Oubilla <malikaoubilla@gmail.com>
2023-12-13 09:16:27 -0800

    13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- number of shows per genre
+SELECT g.name AS genre, COUNT(g.name) AS number_of_shows
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS t
+ON t.genre_id = g.id
+GROUP BY g.id
+ORDER BY number_of_shows;
