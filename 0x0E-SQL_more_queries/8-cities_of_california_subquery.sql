ccc82702886b04a09cdb89067ea25906debc74dd
Malika Oubilla <malikaoubilla@gmail.com>
2023-12-13 08:04:30 -0800

    7, 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- select cities of California
-SELECT * FROM cities WHERE state_id IN (
+SELECT id, name FROM cities WHERE state_id IN (
     SELECT id FROM states WHERE name = 'California'
     ) ORDER BY id;
     
