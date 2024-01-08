8d63d5f6410e8752b60fddbe5769349161acb7a7
Malika <malikaoubilla@gmail.com>
2024-01-08 08:34:04 -0800

    7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const value = parseInt(process.argv[2]);
+if (!isNaN(value)) {
+  for (let i = 0; i < value; i++) {
+    console.log("C is fun");
+}
+}
+else {
+    console.log('Missing number of occurrences');
+}
