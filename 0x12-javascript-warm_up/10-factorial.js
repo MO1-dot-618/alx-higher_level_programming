edcf4ba668d415b3f6f43a6efbafd6b8d942a7ad
Malika <malikaoubilla@gmail.com>
2024-01-08 11:53:48 -0800

    10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const n1 = parseInt(process.argv[2]);
+
+function factorial(n) {
+  if (n === 1 || !n) {
+    return 1;
+  }
+  else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(n1));
