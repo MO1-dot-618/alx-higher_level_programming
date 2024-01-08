94661610a18ef6de1b3dfe63c75c0c5d6f31100f
Malika <malikaoubilla@gmail.com>
2024-01-08 11:36:59 -0800

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const value = parseInt(process.argv[2]);
+if (!isNaN(value)) {
+  for (let i = 0; i < value; i++) {
+    let row = '';
+    for (let j = 0; j < value; j++) {
+        row += 'X';
+    }
+    console.log(row);
+  }
+}
+else {
+    console.log('Missing size');
+}
