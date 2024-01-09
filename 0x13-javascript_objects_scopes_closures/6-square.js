8a49f5167286ef665316403ee3d171f7d77f9873
Malika <malikaoubilla@gmail.com>
2024-01-09 12:02:17 -0800

    56

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    const character = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(character.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
+
