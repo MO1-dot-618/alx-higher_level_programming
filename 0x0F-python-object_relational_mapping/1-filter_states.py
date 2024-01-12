ac3ef4f99afcde29ca2ca153b21435e62ffe8dde
Malika <malikaoubilla@gmail.com>
2024-01-12 04:31:03 -0800

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""listing states"""
+
+
+import MySQLdb
+import sys
+
+
+def listing(username, password, db_name):
+    """function to list states"""
+
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password,
+        db=db_name, charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    if (len(sys.argv)) == 4:
+        listing(sys.argv[1], sys.argv[2], sys.argv[3])
