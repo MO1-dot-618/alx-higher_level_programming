e34eee027bb3c38c7b9aba30a8ea2b18ae3dc4a1
Malika <malikaoubilla@gmail.com>
2024-01-12 06:07:28 -0800

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""listing states"""
+
+
+import MySQLdb
+import sys
+
+
+def listing(username, password, db_name, name):
+    """function to list states"""
+
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password,
+        db=db_name, charset="utf8"
+    )
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE CONVERT(name USING Latin1) \
+    COLLATE Latin1_General_CS = %s ORDER BY id ASC"
+    cur.execute(query, (name,)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    if (len(sys.argv)) == 5:
+        listing(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
